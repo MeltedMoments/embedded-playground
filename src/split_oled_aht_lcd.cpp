6c75e8f09438ffbd8b004e4e0304215db80f479f
MeltedMoments <101322078+MeltedMoments@users.noreply.github.com>
2026-09-15 14:39:19 +0200

    tidy up

--- src/split_oled_aht_lcd.cpp
+++ src/split_oled_aht_lcd.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 #include <Wire.h>
-// #include <rgb_lcd.h>
-// #include <Adafruit_AHTX0.h>
 #include <algorithm>
 #include  <Adafruit_GFX.h>
 #include  <Adafruit_SH110X.h>
@@ -21,31 +19,10 @@ constexpr int OLED_RST  = 8;        // reset
 constexpr int OLED_WIDTH = 64;  
 constexpr int OLED_HEIGHT  = 128; 
 
-// constexpr int SDA_PIN = 4;
-// constexpr int SCL_PIN = 6;
-// constexpr int LCD_COLS = 16;
-// constexpr int LCD_ROWS = 2;
-
-// Sensor intervals
-// constexpr unsigned long HEARTBEAT_INTERVAL_MS = 1000;
-// constexpr unsigned long SENSOR_INTERVAL_MS = 2000;
-// constexpr unsigned long DISPLAY_INTERVAL_MS = 10000;
-// constexpr unsigned long START_DISPLAY_DELAY_MS = 1000;
-
-// // Comfort calculation
-// constexpr float IDEAL_TEMPERATURE   = 23.0;
-// constexpr float IDEAL_HUMIDITY      = 45.0;
-// constexpr float TEMPERATURE_PENALTY = 4.0;
-// constexpr float HUMIDITY_PENALTY    = 0.9;
-
 unsigned long last_heartbeat_time = 0;
-// unsigned long last_sensor_time = 0;
 unsigned long last_display_time = 0;
-// float current_temperature;
-// float current_humidity;
-// bool aht_ready = false;
 bool heartbeat_on = false;
-// bool have_reading = false;
+
 Adafruit_SH1107 display(
     OLED_WIDTH,
     OLED_HEIGHT,
@@ -55,8 +32,6 @@ Adafruit_SH1107 display(
     OLED_RST,
     OLED_CS
 );
-// Adafruit_AHTX0 aht;
-// rgb_lcd lcd; 
 
 const unsigned char heart_8x8[] PROGMEM = {
     0b01100110,
@@ -102,24 +77,6 @@ const unsigned char heart_12x12[] PROGMEM = {
 //     0b000000000000
 // };
 
-// void init_aht20() {
-//     Wire.begin(SDA_PIN, SCL_PIN);
-//     Wire.setTimeOut(100);
-//     aht_ready = aht.begin(&Wire);
-//     if (! aht_ready) {
-//         Serial.println("AHT20 init failed");
-//         return;
-//     }
-//     Serial.println("AHT20 ready");
-// }
-
-// void init_lcd() {
-//     lcd.begin(LCD_COLS, LCD_ROWS);
-//     lcd.setCursor(0, 0);
-//     lcd.print("Hello ESP");
-//     lcd.setCursor(0, 1);
-//     lcd.print("Ready");
-// }
 
 void init_oled() {
     Serial.println("Starting OLED");
@@ -140,7 +97,6 @@ void init_oled() {
 void setup() {
     Serial.begin(115200);
     delay(3000);
-    // init_aht20();
     setup_climate_sensor();
     setup_lcd_display();
     init_oled();
@@ -148,10 +104,6 @@ void setup() {
     last_display_time = millis() - (DISPLAY_INTERVAL_MS - START_DISPLAY_DELAY_MS);
 }
 
-// void lcd_heartbeat() {
-//     lcd.setCursor(15, 1);
-//     lcd.print(heartbeat_on ? '.' : ' ');
-// }
 
 void oled_heartbeat() {
     display.setTextColor(SH110X_WHITE);
@@ -184,44 +136,6 @@ void heartbeat() {
     oled_heartbeat();
     last_heartbeat_time = now;
 }
-
-// // V.Good: 100, Good: 70-99, Okay: 40-69, Bad: 10-39, V.Bad: 0-9
-// String comfort_text (float comfort) {
-//     if (comfort >= 100.0) return "V.Good";
-//     if (comfort >= 70.0)  return "Good  ";
-//     if (comfort >= 40.0)  return "Okay  ";
-//     if (comfort >= 10.0)  return "Bad   ";
-//     return "V.Bad ";
-// }
-
-// float calculate_comfort_score(float temperature, float humidity) {
-//     float score = 100.0;     // best possible
-//     // Penalise the score for every PENALTY away from the ideal, 
-//     // for both temp and humidity
-//     score -= abs(temperature - IDEAL_TEMPERATURE) * TEMPERATURE_PENALTY;
-//     score -= abs(humidity - IDEAL_HUMIDITY) * HUMIDITY_PENALTY;
-
-//     score = std::max(0.0F, score);
-//     score = std::min(100.0F, score);
-//     return score;
-// }
-
-// void show_lcd(float temperature, float humidity, float comfort) {
-//     // Line 1
-//     String buffer = "T:";
-//     buffer += String(temperature, 1);
-//     buffer += "C H:";
-//     buffer += String(humidity, 1);
-//     buffer += "%";
-//     lcd.setCursor(0, 0);
-//     lcd.print(buffer);
-
-//     // Line 2 
-//     buffer = "Comfort: ";
-//     buffer += comfort_text(comfort);
-//     lcd.setCursor(0,1);
-//     lcd.print(buffer);
-// }
 
 void show_oled(float temperature, float humidity, float comfort) {
     display.clearDisplay();
@@ -277,21 +191,6 @@ void update_display() {
     show_oled(current_temperature, current_humidity, comfort);
     last_display_time = now;
 }
-
-// void read_sensor() {
-//     unsigned long now = millis();
-//     if (now - last_sensor_time < SENSOR_INTERVAL_MS) {
-//         return;
-//     }
-
-//     sensors_event_t humidity;
-//     sensors_event_t temperature;
-//     aht.getEvent(&humidity, &temperature);
-//     current_temperature = temperature.temperature;
-//     current_humidity = humidity.relative_humidity;
-//     have_reading = true;
-//     last_sensor_time = now;
-// }
 
 void loop() {
     if (! aht_ready) {
